5de0972550c9eceb3ad9ee6a739d93f3a9edf7c9
Roman-Ojha <razzroman98@gmail.com>
2023-07-31 21:59:25 +0545

    pref: removed all unwanted repo, service & api from customer service

---
+++ microservice/customer/src/api/app-event.js
@@ -0,0 +1,10 @@
+const CustomerService = require("../services/customer-service");
+
+module.exports = (app) => {
+  const service = new CustomerService();
+
+  app.use("/app-events", async (req, res, next) => {
+    const { payload } = req.body;
+    service.SubscribeEvents(payload);
+  });
+};
