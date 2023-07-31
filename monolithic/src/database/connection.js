8cf8f358242b6dc8bb379d65d8c4ce2399beb3e9
Roman-Ojha <razzroman98@gmail.com>
2023-07-31 20:42:51 +0545

    feat: init project for microservice architecture

--- monolithic/src/database/connection.js
+++ monolithic/src/database/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../config");
 
-console.log(DB_URL);
-
 module.exports = async () => {
   try {
     await mongoose.connect(DB_URL, {
